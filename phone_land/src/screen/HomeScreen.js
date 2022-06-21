1537a57cc1b6582a6a98f92648cf1c698ba3f99f
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-06-21 19:28:17 +0100

    📈 updete the home screen (linare gradina)

--- phone_land/src/screen/HomeScreen.js
+++ phone_land/src/screen/HomeScreen.js
@@ -1,4 +1,3 @@
-
 import React, {useState} from 'react';
 import {
   View,
@@ -7,8 +6,11 @@ import {
   ScrollView,
   ImageBackground,
   TextInput,
+  StyleSheet,
   TouchableOpacity,
 } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+
 
 import Header from '../utils/Header';
 import Crard  from '../utils/Card';
@@ -18,7 +20,7 @@ export default function Home({navigation}) {
 
   return (
     <ScrollView style={{padding: 20}}>
-    <SafeAreaView  style={{flex: 1}}>
+       <SafeAreaView  style={{flex: 1}}>
       
       <View
           style={{
@@ -32,13 +34,13 @@ export default function Home({navigation}) {
           </Text>
           <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
             <ImageBackground 
-              source={require('../../assets/Logo.png')}
+              source={require('../assets/image/profile1.jpg')}
                 style={{width: 40, height: 40}}
                 imageStyle={{borderRadius: 25}}
               backgroundColor= '#000'
             />
           </TouchableOpacity>
-        </View  >
+        </View >
         <Text  style={{fontSize: 14, fontFamily: 'poppin-regular' , color: '#A386AF',marginBottom: 5 }}>what do you wanna buy today?</Text>
         <Header/>
         <Crard />
@@ -99,7 +101,7 @@ export default function Home({navigation}) {
                         fontFamily: "poppin-meduim",
                         color: "#A386AF", }}>Tablet</Text>
                   <ImageBackground 
-                      source={require('../assets/image/phone.png')}
+                      source={require('../assets/image/tablet.png')}
                       style={{width: 50, height: 60, marginLeft: 10, bottom: 20}}/>
                 
         </View>
@@ -118,9 +120,9 @@ export default function Home({navigation}) {
                         left: 100,
                         fontSize: 16,
                         fontFamily: "poppin-meduim",
-                        color: "#A386AF", }}>Pc</Text>
+                        color: "#A386AF", }}>Earpud</Text>
                   <ImageBackground 
-                      source={require('../assets/image/phone.png')}
+                      source={require('../assets/image/earbud.png')}
                       style={{width: 50, height: 60, marginLeft: 10, bottom: 20}}/>
                 
         </View>
@@ -141,7 +143,7 @@ export default function Home({navigation}) {
                         fontFamily: "poppin-meduim",
                         color: "#A386AF", }}>Watch</Text>
                   <ImageBackground 
-                      source={require('../assets/image/phone.png')}
+                      source={require('../assets/image/watch.png')}
                       style={{width: 50, height: 60, marginLeft: 10, bottom: 20}}/>
                 
         </View>
@@ -150,18 +152,83 @@ export default function Home({navigation}) {
             flexDirection: 'row',
             justifyContent: 'space-between',
             marginTop:10
-            
-            
           }}>
         <Text  style={{fontSize: 18, fontFamily: 'poppin-bold' , color: '#400C56' }}>Popular Brands</Text>
         <Text  style={{fontSize: 14, fontFamily: 'poppin-meduim' , color: '#A386AF' }} onPress={() => navigation.navigate("Brand")}>See All</Text>
         </View>
         <ScrollCaty/>
         </SafeAreaView>
+        <SafeAreaView style={{ flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+             }} >
+        <View style={styles.container} >
+      <LinearGradient
+          colors={['#35F0C0', '#E89A69' ]}
+          style={styles.linearGradient}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 2, y: 0.5 }}
+        />
+        <Text style={styles.titel}  >Xiaomi</Text>
+        <Text style= {styles.descrption}>Mi Band 6</Text>
+        <ImageBackground 
+                      source={require('../assets/image/watch.png')}
+                      style={{width: 80, height: 60, marginLeft: 10, position:'absolute',top: 65, left: 22}}/>
+        
+    </View>
+    <View style={styles.container} >
+      <LinearGradient
+          colors={['#007BEB', '#E89A69' ]}
+          style={styles.linearGradient}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 2, y: 0.5 }}
+        />
+        <Text style={styles.titel}  >Apple</Text>
+        <Text style= {styles.descrption}>Iphone 12</Text>
+        <ImageBackground 
+                      source={require('../assets/image/phone.png')}
+                      style={{width: 75, height: 60, marginLeft: 10, position:'absolute',top: 65, left: 22}}/>
+        
+    </View>
+        </SafeAreaView>
+       
       </ScrollView>
-  
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    alignItems: "flex-start",
+    position: 'relative',
+  },
+  linearGradient: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 150,
+    marginVertical: 15,
+    height:150,
+    borderRadius: 20
+  },
+  titel: {
+    position:'absolute',
+    top: 25,
+    left: 50,
+    fontSize: 12,
+    fontFamily: "poppin-regular",
+    color: "#fff",
+  },
+  descrption: {
+    position:'absolute',
+    top: 40,
+    left: 35,
+    fontSize: 15,
+    width:200,
+    fontFamily: "poppin-bold",
+    color: "#fff",
+    
+  }
+})
 
 
 
