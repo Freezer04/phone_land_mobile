1d4ca5543a4a28f6bd2acca11946aca51ae2d238
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-06-25 16:29:43 +0100

    ♻️ refactry code

--- phone_land/src/utils/ScrollCaty.js
+++ phone_land/src/utils/ScrollCaty.js
@@ -1,116 +1,109 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {View, StyleSheet, ScrollView, Text} from 'react-native';
+import {View, StyleSheet, ScrollView, Text, TouchableOpacity, ImageBackground} from 'react-native';
 import Sony from "../../assets/image/sony.svg"
 
-const ScrollCaty = () => {
-  
-  const [ Data, setData ] = useState([])
-  
-    useEffect(() => {
-      axios.get('http://192.168.137.1:3000/api/brand')
-      .then(res => {
-          // console.log(res.data);
-          setData(res.data);
-      }).catch (err => {
-          console.log(err);
-          console.log('makin walo');
-      }) 
-  },[]) 
+const ScrollCaty = ({navigation}) => {
+
      
   return (
     <ScrollView horizontal={true}  showsHorizontalScrollIndicator={false} >
     
-     <View style={{
+     <TouchableOpacity style={{
           alignItems:"center",
           justifyContent: "center",    
           borderColor: '#A386AF',
           backgroundColor: "#fff",
-          borderWidth: 1,
+          borderWidth: 0.5,
           borderRadius: 16,
           width: 150,
           height: 40, 
-          marginHorizontal: 5 }} >
-                    
-                    <Text style={{  
+          marginHorizontal: 5 }} onPress={() => navigation.navigate('Brand')} >
+            <ImageBackground source={require('../assets/image/Apple.png')} style={{ width:20, height:25 }}/>
+                    {/* <Text style={{  
                       fontSize: 14,
                       fontFamily: "poppin-meduim",
                       color: "#400C56",
                        }}>Apple
-                    </Text>      
-      </View>
-      <View style={{
+                    </Text>       */}
+      </TouchableOpacity>
+      <TouchableOpacity style={{
           alignItems:"center",
           justifyContent: "center",    
           borderColor: '#A386AF',
           backgroundColor: "#fff",
-          borderWidth: 1,
+          borderWidth: 0.5,
           borderRadius: 16,
           width: 150,
           height: 40, 
-          marginHorizontal: 5 }} >
-                    
-                    <Text style={{  
+          marginHorizontal: 5 }} onPress={() => navigation.navigate('Brand')} >
+            <ImageBackground source={require('../assets/image/Xiaomi.png')} style={{ width:28, height:25 }}/>
+                    {/* <Text style={{  
                       fontSize: 14,
                       fontFamily: "poppin-meduim",
                       color: "#400C56",
                        }}>Xiaomi
-                    </Text>      
-      </View>
-      <View style={{
+                    </Text>       */}
+      </TouchableOpacity>
+      <TouchableOpacity style={{
           alignItems:"center",
           justifyContent: "center",    
           borderColor: '#A386AF',
           backgroundColor: "#fff",
-          borderWidth: 1,
+          borderWidth: 0.5,
           borderRadius: 16,
           width: 150,
           height: 40, 
-          marginHorizontal: 5 }} >
-                    
-                    <Text style={{  
+          marginHorizontal: 5 }} onPress={() => navigation.navigate('Brand')} >
+                <ImageBackground source={require('../assets/image/samsung.png')} style={{ width:45, height:35 }}/>
+                    {/* <Text style={{  
                       fontSize: 14,
                       fontFamily: "poppin-meduim",
                       color: "#400C56",
                        }}>Sammsung
-                    </Text>      
-      </View>
-      <View style={{
+                    </Text>       */}
+      </TouchableOpacity>
+      <TouchableOpacity style={{
           alignItems:"center",
           justifyContent: "center",    
           borderColor: '#A386AF',
           backgroundColor: "#fff",
-          borderWidth: 1,
+          borderWidth: 0.5,
           borderRadius: 16,
           width: 150,
           height: 40, 
-          marginHorizontal: 5 }} >
-                    
-                    <Text style={{  
+          marginHorizontal: 5 }} onPress={() => navigation.navigate('Brand')} >
+                <ImageBackground source={require('../assets/image/oppo.png')} style={{ width:50, height:34 }}/>
+                    {/* <Text style={{  
                       fontSize: 14,
                       fontFamily: "poppin-meduim",
                       color: "#400C56",
                        }}>Oppo
-                    </Text>      
-      </View>
-      <View style={{
+                    </Text>       */}
+      </TouchableOpacity>
+      <TouchableOpacity style={{
+           flexDirection: 'row',
+          //  justifyContent: ,
           alignItems:"center",
           justifyContent: "center",    
           borderColor: '#A386AF',
           backgroundColor: "#fff",
-          borderWidth: 1,
+          borderWidth: 0.5,
           borderRadius: 16,
           width: 150,
           height: 40, 
-          marginHorizontal: 5 }} >
-                    
+          marginHorizontal: 5 }} onPress={() => navigation.navigate('Brand')} >
+              <ImageBackground source={require('../assets/image/sony.png')} style={{ width: 45, height:25 }}/>
+                    {/* <View>
                     <Text style={{  
-                      fontSize: 14,
+                      fontSize: 10,
                       fontFamily: "poppin-meduim",
                       color: "#400C56",
-                       }}>One Plus
-                    </Text>      
-      </View>
+                       }}>One Pluss
+                    </Text>
+                    </View> */}
+                   
+      </TouchableOpacity>
         
   </ScrollView>
   );
